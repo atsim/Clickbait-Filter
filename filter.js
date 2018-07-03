b27f1a5ad438d948a64f0fc7d23d443ddc91f694
Antony Ting Chung Tsim <atctsim@uwaterloo.ca>
2018-07-03 17:42:57 -0400

    Fixed so base functionality works

--- filter.js
+++ filter.js
@@ -1,6 +1,7 @@
 let ratio = 0.75;
 
 let running = false;
+let skip = false;
 
 var observer = new MutationObserver(callback);
 var buzzwords = ['amazing', 'must watch', 'not believe', 'super lucky', 'crazy']
@@ -21,34 +22,41 @@ function callback(mutations) {
   $("ytd-thumbnail").each(function (index) {
     if (pathname == '/results') {
       nameCheck = $("ytd-thumbnail").eq(index).next().children().first().children().first().children().first().children().eq(1);
-    } else {
+    } else if (!$("ytd-thumbnail").eq(index).next().children().first().children().eq(1).is("span")){
+      // console.log($("ytd-thumbnail").eq(index).next().children().first().children().first().children().eq(1).text());
       nameCheck = $("ytd-thumbnail").eq(index).next().children().first().children().first().children().eq(1);
+    } else {
+      skip = true;
     }
 
-    let numUpper = (nameCheck.text().match(/[A-Z]/g) || []).length;
+    if (skip == false) {
+    let numUpper = (nameCheck.text().match(/[A-Z]/g)).length;
+    //console.log(numUpper);
     let titleRatio = numUpper/nameCheck.text().match(/[a-zA-Z]/g).length;
-    let buzzwordsCheck = /.*(amazing|must watch|not believe|super lucky|crazy).*/.test(nameCheck.toLowerCase());
-
+    //let buzzwordsCheck = /.*(amazing|must watch|not believe|super lucky|crazy).*/.test(nameCheck.toLowerCase());
 
     // Check if title fits clickbait criteria
-    if (nameCheck.text() != "" && (titleRatio >= ratio || (titleRatio >= 0.6 && buzzwords exist)) && nameCheck.attr("data-clickbait") != 'true') {
+    if (nameCheck.text() != "" && (titleRatio >= ratio) && nameCheck.attr("data-clickbait") != 'true') {
+      console.log("Added append to: " + nameCheck.text())
       nameCheck.text("(Clickbait) " + nameCheck.text());
       nameCheck.attr("data-clickbait", 'true');
       $("ytd-thumbnail").eq(index).addClass("clickbait");
     }
+  }
     
   });
   running = false;
+  skip = false;
 }
 
 chrome.history.onVisited.addListener(function(details) {
-  alert("qwer");
-    if(details.frameId === 0) {
-        // Fires only when details.url == currentTab.url
-        chrome.tabs.get(details.tabId, function(tab) {
-            if(tab.url === details.url) {
-                console.log("onHistoryStateUpdated");
-            }
-        });
-    }
+  console.log("URL Updated");
+  if(details.frameId === 0) {
+    // Fires only when details.url == currentTab.url
+    chrome.tabs.get(details.tabId, function(tab) {
+      if(tab.url === details.url) {
+        console.log("onHistoryStateUpdated");
+      }
+    });
+  }
 });

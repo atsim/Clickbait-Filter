ecd1b2318341fdc7b7fce6ce9516b1b894a56b57
Antony Ting Chung Tsim <atctsim@uwaterloo.ca>
2019-01-05 23:03:50 -0500

    Added ability for users to put their own threshold + deprecated old pages

--- filter.js
+++ filter.js
@@ -1,12 +1,20 @@
 let ratio = 0.75;
 
+
+chrome.storage.sync.get("threshold", function(obj) {
+  console.log(obj);
+  console.log(!Object.keys(obj).length === 0);
+  console.log("Saved threshold: " + obj.threshold);
+  if (Object.keys(obj).length !== 0) {
+    ratio = parseFloat(obj.threshold);
+  }
+  console.log("RATIO: " + ratio);
+});
+console.log("RATIO: " + ratio);
 let running = false;
 let skip = false;
 
 var observer = new MutationObserver(callback);
-//var buzzwords = ['amazing', 'must watch', 'not believe', 'super lucky', 'crazy']
-
-
 observer.observe(document.getElementById("contents"), {
     childList: true,
     subtree: true            
@@ -22,7 +30,6 @@ function callback(mutations) {
 
   $('[id="video-title"]').each(function (index) {
     nameCheck = $('[id="video-title"]')[index];
-
     if (nameCheck.nodeName == 'A') {
       let numUpper = 0;
       let titleRatio = 0;
@@ -31,8 +38,9 @@ function callback(mutations) {
         titleRatio = numUpper / nameCheck.title.match(/[a-zA-Z]/g).length;
       }
 
+
       // Check if title fits clickbait criteria
-      if (nameCheck.title != "" && (titleRatio >= ratio) && !nameCheck.hasAttribute("data-clickbait")) {
+      if (nameCheck.title != "" && (parseFloat(titleRatio) >= parseFloat(ratio)) && !nameCheck.hasAttribute("data-clickbait")) {
         console.log("Added append to: " + nameCheck.title);
         nameCheck.innerText = "(Clickbait) " + nameCheck.title;
         nameCheck.setAttribute("data-clickbait", 'true');
@@ -40,7 +48,6 @@ function callback(mutations) {
         // Select thumbnail element
         let thumbnail = '';
         if (pathname == '/results') {
-          //console.log(nameCheck.parentElement.parentElement.parentElement.parentElement.previousElementSibling);
           thumbnail = nameCheck.parentElement.parentElement.parentElement.parentElement.previousElementSibling;
           thumbnail.className += " clickbait";
         } else {
--- options.js
+++ options.js
@@ -1,35 +1,16 @@
 let page = document.getElementById('thresholdDiv');
 
-/*
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
-    button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log('color is ' + item);
-      })
-    });
-    page.appendChild(button);
-  }
-}
-
-constructOptions(kButtonColors);
-*/
-
 function constructThreshold () {
   let threshInput = document.createElement('input');
   threshInput.setAttribute('id', 'threshInput');
   page.appendChild(threshInput);
-
   let threshSubmit = document.createElement('button');
   threshSubmit.setAttribute('id', 'threshSubmit');
   threshSubmit.setAttribute('value', 'Submit');
   threshSubmit.innerHTML = 'Submit';
   threshSubmit.addEventListener('click', function() {
-    chrome.storage.sync.set({threshold: $("#threshInput").val()}, function() {
-      console.log("Set threshold to " + $("#threshInput").val());
+    chrome.storage.sync.set({"threshold": document.getElementById("threshInput").value}, function() {
+      console.log("Set threshold to " + document.getElementById("threshInput").value);
     })
   });
   page.appendChild(threshSubmit);
